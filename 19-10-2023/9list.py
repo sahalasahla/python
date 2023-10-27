6939d2e7a15cdf980ca0256ae8be032825a80701
sahalasahla <145668144+sahalasahla@users.noreply.github.com>
2023-10-27 15:53:47 +0530

    Add files via upload

---
+++ 19-10-2023/9list.py
@@ -0,0 +1,9 @@
+list=[]
+n=int(input("Enter number of elements:"))
+for i in range(0,n):
+    ele=input("Enter color:")
+    list.append(ele)
+    print(list)
+print("The first and last elements are:",list[0],list[-1])
+
+    
