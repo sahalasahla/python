e7701d23d162d0420f152690793b76f241ad8f8d
sahalasahla <145668144+sahalasahla@users.noreply.github.com>
2023-11-02 16:28:48 +0530

    Add files via upload

---
+++ 26-10-2023/area.py
@@ -0,0 +1,14 @@
+a=int(input("enter the side of the square\n"))
+area=lambda b:b*b
+print("area :",area(a))
+
+a=int(input("Enter the length of rectangle"))
+b=int(input("Enter the breadth of rectangle"))
+area=lambda l,b:1*b
+print("Area of the rectangle is",area(a,b))
+
+
+a=int(input("Enter the height of the triangle"))
+b=int(input("Enter the breadth of the triangle"))
+area=lambda h,b:h*b
+print("Area of the triangle is",area(a,b))
