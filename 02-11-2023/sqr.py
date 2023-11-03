5c1bc6fd4e17d18e7251da58c8828d46bff216c1
sahalasahla <145668144+sahalasahla@users.noreply.github.com>
2023-11-03 16:24:47 +0530

    Add files via upload

---
+++ 02-11-2023/sqr.py
@@ -0,0 +1,25 @@
+list=[]
+def square(a,b):
+    #list=[]
+    for i in range(a,b+1):
+        j=1;
+        while j*j<=i:
+            if j*j==i:
+                 list.append(str(i))
+            j=j+1
+        i=i+1
+    return list
+print(square(1000,9999))
+
+list1=[]
+for i in list:
+    count=0
+    for z in i:
+        if int(z)%2==0:
+            count=count+1
+        else:
+            break
+    if count==4:
+        list1.append(int(i))
+print(list1)
+        
