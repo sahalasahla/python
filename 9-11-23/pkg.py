eface3e5a653b75d9d9d8be3a70088de7545f70c
sahalasahla <145668144+sahalasahla@users.noreply.github.com>
2023-11-16 14:43:06 +0530

    Add files via upload

---
+++ 9-11-23/pkg.py
@@ -0,0 +1,10 @@
+from package import sum as sum1
+from package import pwr as pwr1
+from package import average as average1
+
+a=sum1.sum(10,20)
+b=average1.average(10,20)
+c=pwr1.power(2,3)
+print(a)
+print(b)
+print(c)
